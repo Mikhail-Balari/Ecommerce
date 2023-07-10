71d68f3f7d126d8702ebcb6b66cba885b289586f
Mikhail-Balari <balarimikhail@gmail.com>
2023-07-10 12:40:49 -0300

    Header.jsx Colors.js

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { colors } from '../Global/Colors'
 
 const Header = () => {
   return (
     <View style={styles.containerHeader}>
-      <Text>Header</Text>
+      <Text style={styles.textHeader}>Header</Text>
     </View>
   )
 }
@@ -14,6 +15,12 @@ export default Header
 const styles = StyleSheet.create({
     containerHeader: {
         height: '10%',
-        backgroundColor: 'orange',
+        backgroundColor: colors.darkmatter,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    textHeader: {
+        fontSize: 25,
+        color: colors.secondary,
     }
 })
---
+++ src/Global/Colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    primary: "#FF8303",
+    secondary: "#F0E3CA",
+    coffee: "#A35709",
+    darkmatter: "#1B1A17"
+}
