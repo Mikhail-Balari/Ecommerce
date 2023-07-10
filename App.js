748d6318fbd502bab01a86022d90d2ebcc9044ca
Mikhail-Balari <balarimikhail@gmail.com>
2023-07-10 12:56:39 -0300

    Call categories.json from Home.jsx

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/Components/Header';
+import Home from './src/Screens/Home';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Header/>
+      <Home/>
     </View>
     
   );
--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { colors } from '../Global/Colors'
+import categories from '../Data/categories.json'
 
 const Home = () => {
   return (
-    <View>
-      
+    <View style={styles.container}>
+        <FlatList
+            data = {categories}
+            keyExtractor={category => category}
+            renderItem={({item}) => {
+                return (
+                    <Text>{item}</Text>
+                )
+            }}
+        />
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        height: '90%',
+        backgroundColor: colors.primary, 
+    }
+})
