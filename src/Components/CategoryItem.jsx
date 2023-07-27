80df904df85cf4d598822ed5dd80343be304a82f
Mikhail-Balari <balarimikhail@gmail.com>
2023-07-26 21:18:03 -0300

    CategoryItem.jsx dispatch setCategorySelected

--- src/Components/CategoryItem.jsx
+++ src/Components/CategoryItem.jsx
@@ -1,12 +1,22 @@
 import { Pressable, StyleSheet, Text, useWindowDimensions, View} from 'react-native'
 import React from 'react'
 import Card from './Card'
+import { useDispatch } from 'react-redux'
+import { setCategorySelected } from '../Features/Shop/shopSlice'
 
 const CategoryItem = ({item, navigation}) => {
+
   const {width} = useWindowDimensions()
+
+  const dispatch = useDispatch()
+
+  const onSelectCategory = () => {
+    dispatch(setCategorySelected(item))
+    navigation.navigate('ItemListCategory', {category: item})
+  }
   return (
     <View style = {{width:width, alignItems: 'center'}}>
-      <Pressable onPress={() => navigation.navigate('ItemListCategory', {category: item})}>
+      <Pressable onPress={onSelectCategory}>
           <Card>
               <Text style={styles.categoryText}>{item}</Text>
           </Card>
